a70136e8aeba8dd56811749976558cf23ff00e3d
HDD-WoundAI <nengenheiro@gmail.com>
2026-04-06 09:39:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if imagem:
 # ========================
 # IA
 # ========================
-if st.button("🧠 Análise automática") and imagem:
+ imagem:
 
     base64_image = base64.b64encode(bytes_data).decode("utf-8")
 
